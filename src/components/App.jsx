5626d26ade814463dcca797deb8ffa113c55683b
Bobby Ayers <rayers@quavermusic.com>
2016-10-18 13:57:22 -0500

    finished up to creation of Action Creators

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {List, Map } from 'immutable';
+
+
+
+export default React.createClass({
+    render: function() {
+        return this.props.children;
+    }
+});
